9934d1447f46ddd7d1e45b91cb48bc9c67a7d556
Neal Beeken <neal.beeken@mongodb.com>
2022-05-16 15:42:40 -0400

    Add mdb

--- api/hello.js
+++ api/hello.js
@@ -1,10 +1,17 @@
+import { MongoClient } from 'mongodb'
+
 export default function handler(
   request,
   response,
 ) {
-  response.status(200).json({
-    body: request.body,
-    query: request.query,
-    cookies: request.cookies,
-  });
+  const client = new MongoClient(process.env.MONGODB_URI)
+
+  await client.connect()
+
+  const db = client.db('sample_mflix')
+  const movies = db.collection('movies')
+
+  const films = await movies.find({}).limit(10).toArray()
+
+  response.status(200).json({ hello: 1, films });
 }
--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 async function askServer() {
-    const response = await fetch('https://node-4248-kw2uayr2z-nealbeeken-mongodbcom.vercel.app/api/hello')
+    const response = await fetch('api/hello')
     const decoded = await response.json()
     console.dir(decoded)
 }
